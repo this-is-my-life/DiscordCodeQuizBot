436863636059ca358126fa5d7558afe870e3665f
PMHStudio <pmhstudio.pmh@gmail.com>
2019-02-17 13:15:26 +0900

    [02/17] Bot Basic Added

---
+++ Discord/CodeQuiz.js
@@ -0,0 +1,78 @@
+/* eslint-disable no-console */
+/*
+  proj- Keep Typing and Nobody Explodes. The Discord Bot,
+
+  We Don't Like Close-Source. Email: pmhstudio.pmh@gmail.com
+*/
+
+const discord = require('discord.js')
+const QuizData = require('../QuizData/QuizData.json')
+
+const botToken = process.env.Token
+const bot = new discord.Client()
+
+bot.login(botToken)
+
+bot.on('ready', () => {
+  console.log(bot.user.username + ' is Booted!')
+  bot.user.setActivity("Dev | 'cq!'를 입력해 퀴즈를 출력합니다!")
+})
+
+bot.on('message', (input) => {
+  if (input.author.id === bot.user.id) { return }
+  let filter = (reaction, user) => (reaction.emoji.name === '⭕' || reaction.emoji.name === '❌') && user.id === input.author.id
+
+  // Message Caculation.
+  let msgArray = input.content.split(' ')
+  if (msgArray[0] === 'cq!' || msgArray[0] === 'codequiz!') {
+    let quizNum = Math.floor(Math.random() * (QuizData.length - 1))
+    let quizEmbed = new discord.RichEmbed()
+      .setColor(0x0000ff)
+      .setAuthor(input.author.username + '님이 Code Quiz를 풀고있습니다', input.author.displayAvatarURL)
+      .setTitle('Quiz No.' + quizNum)
+      .addField('Q. ' + QuizData[quizNum].question, '제한시간 **1분**')
+    input.channel.send(quizEmbed).then((th) => {
+      th.react('⭕')
+      th.react('❌')
+      th.awaitReactions(filter, {
+        time: 60000,
+        max: 1
+      }).then((collected) => {
+        if (!collected) {
+          let quizFailByLate = new discord.RichEmbed()
+            .setColor(0x808080)
+            .setDescription('[문제, 정답, 풀이 오류신고, 수정요청, 추가신청](https://github.com/PMHStudio/DiscordCodeQuizBot/issues/new)')
+            .setAuthor(input.author.username + '님이 Code Quiz를 풀지못하셨습니다', input.author.displayAvatarURL)
+            .setTitle('Quiz No.' + quizNum)
+            .addField('Q. ' + QuizData[quizNum].question, '**A.** ' + QuizData[quizNum].explanation)
+          th.edit(quizFailByLate)
+        } else {
+          let QuizAwnser
+          if (QuizData[quizNum].awnser === true) {
+            QuizAwnser = '⭕'
+          } else if (QuizData[quizNum].awnser === false) {
+            QuizAwnser = '❌'
+          }
+
+          if (collected.array()[0].emoji.name === QuizAwnser) {
+            let quizCorrectEmbed = new discord.RichEmbed()
+              .setColor(0x00ff00)
+              .setDescription('[문제, 정답, 풀이 오류신고, 수정요청, 추가신청](https://github.com/PMHStudio/DiscordCodeQuizBot/issues/new)')
+              .setAuthor(input.author.username + '님이 Code Quiz를 맞추셨습니다!', input.author.displayAvatarURL)
+              .setTitle('Quiz No.' + quizNum)
+              .addField('Q. ' + QuizData[quizNum].question, '**A.** ' + QuizData[quizNum].explanation)
+            th.edit(quizCorrectEmbed)
+          } else {
+            let quizNotCorrectEmbed = new discord.RichEmbed()
+              .setColor(0xff0000)
+              .setDescription('[문제, 정답, 풀이 오류신고, 수정요청, 추가신청](https://github.com/PMHStudio/DiscordCodeQuizBot/issues/new)')
+              .setAuthor(input.author.username + '님이 Code Quiz를 풀지못하셨습니다', input.author.displayAvatarURL)
+              .setTitle('Quiz No.' + quizNum)
+              .addField('Q. ' + QuizData[quizNum].question, '**A.** ' + QuizData[quizNum].explanation)
+            th.edit(quizNotCorrectEmbed)
+          }
+        }
+      })
+    })
+  }
+})
---
+++ readme.md
@@ -0,0 +1,3 @@
+# CodeQuiz
+
+[![JavaScript Style Guide](https://cdn.rawgit.com/standard/standard/master/badge.svg)](https://github.com/standard/standard)
