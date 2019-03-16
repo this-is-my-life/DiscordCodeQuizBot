7158d24a119a8fd2d81dabc1a4fe7e52c7a84f95
PMHStudio <pmhstudio.pmh@gmail.com>
2019-03-16 19:40:14 +0900

    Hot Fixed

--- Discord/CodeQuiz.js
+++ Discord/CodeQuiz.js
@@ -32,7 +32,7 @@ bot.on('message', (input) => {
       if (msgArray[1] <= QuizData.quiz.length - 1) {
         quizNum = msgArray[1]
       } else if (QuizData.info.language.includes(msgArray[1])) {
-        quizNum = QuizData.quiz.filter((thing) => thing.language === msgArray[1])
+        quizNum = QuizData.quiz.filter((thing) => thing.language === msgArray[1])[Math.floor(Math.random() * (QuizData.quiz.filter((thing) => thing.language === msgArray[1].length - 1)))]
       }
     }
     let quizEmbed = new discord.RichEmbed()
